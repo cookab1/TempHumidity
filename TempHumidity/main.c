874f1fb52b2a592b13418dc60aba9c82a48e0b19
Andy Cook <andyrock.cook@gmail.com>
2018-03-14 09:46:42 -0400

    init commit

---
+++ TempHumidity/main.c
@@ -0,0 +1,18 @@
+/*
+ * TempHumidity.c
+ *
+ * Created: 3/14/2018 9:45:42 AM
+ * Author : andyr
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
